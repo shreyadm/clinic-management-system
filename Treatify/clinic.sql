2ad2812b7f5de81189ed9854268b98500fe3073b
RashmiMokashi <rashmimokashi2099@gmail.com>
2019-09-13 19:13:02 +0530

    first commit

---
+++ Treatify/clinic.sql
@@ -0,0 +1,9 @@
+create database trying;
+show databases;
+use trial;
+create table stud(n1 int,n2 int);
+insert into stud values(1,2);
+select * from stud;
+insert into stud values(4,5);
+select * from stud; 
+show databases;
